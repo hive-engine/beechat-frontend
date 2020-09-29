9d205c83dcefbc1ac994e6bb5a9781a7d7404555
Reazul Iqbal <admin@reazul.net>
2020-09-29 14:57:21 +0600

    Moved to Live

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,8 @@ module.exports = {
     watch: true,
     env_production: {
       NODE_ENV: 'production',
-      API_BASE_URL: 'https://beechat.dtools.dev/api',
-      WEBSOCKET_URL: 'wss://ws.beechat.dtools.dev'
+      API_BASE_URL: 'https://beechat.hive-engine.com/api',
+      WEBSOCKET_URL: 'wss://ws.beechat.hive-engine.com'
     }
   }]
 }
